e9f1c06cadf907a5375610f7e931618c52e038b2
Danis Nurmansyah <danisnurman@users.noreply.github.com>
2020-05-23 07:55:22 +0700

    Update BankingManagementSystem.java
    Insyaallah sudah final

--- src/BankApp/BankingManagementSystem.java
+++ src/BankApp/BankingManagementSystem.java
@@ -4,19 +4,7 @@ import BankApp.BukaRekening;
 import BankApp.Login;
 import BankApp.Transaksi;
 
-import java.util.*;
-import java.text.*;
-import java.util.Arrays;
-import java.io.*;
-import java.net.*;
-import java.nio.charset.Charset;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.util.Scanner;
 
 class Bank {
@@ -25,13 +13,13 @@ class Bank {
 		int pilihan;
 
 		while(true) {
-			System.out.println("\n-------------------");
-			System.out.println("BANK     OF     PTI");
-			System.out.println("-------------------\n");
-			System.out.println("1. Daftar akun.");
-			System.out.println("2. Masuk.");
-			System.out.println("3. Keluar.");
-			System.out.print("\nPilih menu\t: ");
+			System.out.println("\n--------------------");
+			System.out.println("BANK TABUNGAN RAKYAT");
+			System.out.println("--------------------\n");
+			System.out.println("1. Belum punya rekening? DAFTAR!");
+			System.out.println("2. Login akun");
+			System.out.println("3. Keluar aplikasi");
+			System.out.print("\nPilihan\t: ");
 			pilihan = scan.nextInt(); scan.nextLine();
 			switch (pilihan) {
 				case 1:
@@ -41,11 +29,11 @@ class Bank {
 					Login.login();
 					break;
 				case 3:
-					System.out.println("\nTerima kasih telah menggunakan aplikasi PTI Mobile Banking."); 
+					System.out.println("\nTerima kasih telah menggunakan aplikasi BTR Banking."); 
 					System.exit(1);
 					break;
 				default:
-					System.out.println("Input tidak valid!");
+					System.out.println(">>> Input tidak valid!");
 			}
 		}
 	}
